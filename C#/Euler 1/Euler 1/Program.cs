a97da4ecfb7ad2bd76badf87be202faa5d374535
Jojimbo20 <jordanrodrigues@hotmail.co.uk>
2015-12-02 19:20:23 +0000

    Added Solutions 1 and 4

---
+++ C#/Euler 1/Euler 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Euler1
+{
+    public static void Main(string[] args)
+    {
+        int max = 1000;
+        int sum = 0;
+
+        //Looper that counts up to 1000
+        for (int i = 0; i < max; i++)
+        {
+            //Check to see if it's divisible by 3 or 5
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                Console.WriteLine("{0}", i);
+                sum = (sum + i);
+            }
+        }
+        Console.WriteLine("Solution reached: {0}", sum);
+        Console.ReadLine();
+    }
+}
---
+++ C#/Euler 4/Euler 4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+namespace Euler_4
+{
+    class Program
+    {
+        static bool isPalindrome(int x)
+        {            
+            //I declared two strings, one to store the unchanged number
+            //One to store the reversed number
+            string unReversed = x.ToString();
+            string Reversed = Hello.Reverse(unReversed);
+
+            //Because I can't figure out how to compare strings I'm gonna compare the ints
+            int reversedInt = Convert.ToInt32(Reversed);
+              
+            //If the reversed one and the normal one are th e same it's a palindrome
+               if(x == reversedInt)
+                  {
+                    return (true);
+                  }
+
+            else
+            return (false);
+        }
+        static void Main(string[] args)
+        {
+            int product = 0;
+            int palindrome = 0;
+            //Make a looper that multiplies one 3 digit number by another  
+            for(int a = 1000; a > 100; --a)
+            {
+                for(int b = 1000; b > 100; --b)
+                {
+                    product = a * b;
+
+                    if (isPalindrome(product))
+                    {
+                        if( palindrome < product)
+                        {
+                            palindrome = product;
+                        }
+                        Console.WriteLine("palindrome: {0}", palindrome);
+                        
+                    }
+              
+                    
+                }
+                Console.WriteLine(product);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
